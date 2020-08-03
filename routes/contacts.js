14212040e23e28f7bc0a3902ad9d0132b442f63a
Apple <apple@macbookpro.local>
2020-08-03 18:21:57 +0530

    Other changes in express routes & gitignore

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,31 +1,60 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const User = require('../models/User')
+const Contact = require('../models/Contact')
+const auth = require('../middleware/auth')
+
 
 // @route GET api/contacts
 // @desc  Get all contacts of logged in user
 // @access Private
-router.get('/',(req, res) => 
-    res.send('Get all contacts...')
-)
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({ user: req.user.id }).sort({ date: -1 })
+        res.json(contacts)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error ')
+    }
+})
 
 // @route POST api/contacts
 // @desc  Add new contact
 // @access Private
-router.post('/',(req, res) => 
-    res.send('Add contact...')
+router.post('/', [auth,
+    [check('name', 'name is required').not().isEmpty()]
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.sendStatus(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, phone, type } = req.body
+
+    try {
+        const newContact = new Contact({
+            name, email, phone, type, user: req.user.id
+        })
+        const contact = await newContact.save()
+        res.json(contact)
+    } catch (err) {
+        res.status(500).send('server error ')
+    }
+}
 )
 
 // @route PUT api/contacts/:id
 // @desc  Update the contact
 // @access Private
-router.put('/:id',(req, res) => 
+router.put('/:id', (req, res) =>
     res.send('Update contact ...')
 )
 
 // @route DELETE api/contacts/:id
 // @desc  Delete the contact
 // @access Private
-router.delete('/:id',(req, res) => 
+router.delete('/:id', (req, res) =>
     res.send('Delete contact ...')
 )
 
